42736f9d46dfc5202b18605607281a3ac322ed54
Klaus Gerken <klager88@tzi.de>
2012-01-31 21:03:35 +0100

    Next step: Input Letter and maximum tries 	modified:   test.rb

--- Aufgabe1/test.rb
+++ Aufgabe1/test.rb
@@ -3,5 +3,21 @@ lines = file.readlines
 file.close
 wordArr = lines.to_a
 
-print [rand(dictionary.size)-1].upcase
+
+word = wordArr[rand(wordArr.size)-1].upcase
+found = word.gsub(/./, "_")
+puts word
+
+for i in 0..6 do
+  puts found.gsub(/([A-Z_])/, "\\1 ")
+  input = gets.chomp.upcase
+  if word.index(input) != nil
+    for i in 0...word.size
+      if word[i].chr == input
+        found[i] = input
+      end
+      puts found
+    end
+  end
+end
 
